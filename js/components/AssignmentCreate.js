ff539a604153cc9ed7bd5f1ca5cbfe5a6ce88bfd
AyoubMS <chahayb563@gmail.com>
2023-08-08 15:29:46 +0200

    component responsibility

---
+++ js/components/AssignmentCreate.js
@@ -0,0 +1,27 @@
+export default {
+    template: `
+        <form @submit.prevent="add">
+            <div class="border border-gray-600 text-black">
+                <input v-model="newAssignment" placeholder="New assignment..." class="p-2" />
+                <button type="submit" class="bg-white p-2 border-l">Add</button>
+            </div>
+        </form>
+    `,
+
+    // props: {
+    //     assignments: Function
+    // },
+
+    methods: {
+        add() {
+            this.$emit('add', this.newAssignment);
+            this.newAssignment = ''
+        }
+    },
+
+    data() {
+        return {
+            newAssignment: ''
+        }
+    }
+}
